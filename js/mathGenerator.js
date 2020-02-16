f8fe621605825c32701f0a1a51ca6b0fe392feb1
Keith <keithrvell@gmail.com>
2020-02-17 00:29:06 +0100

    Finished basic version of the website.

--- js/mathGenerator.js
+++ js/mathGenerator.js
@@ -1,36 +1,65 @@
-
 function GenerateNumbers() {
-    console.log("Hello");
-
     //returns a number between -25 and 25, but not 0
+
+    let tdList = $("td");
+    $.each(tdList, function () {
+        if ($(this).attr('class') === "simple") {
+            $(this).html(GenerateString(1));
+        } else if ($(this).attr('class') === "moderate") {
+            $(this).html(GenerateString(2));
+        }else if ($(this).attr('class') === "difficult") {
+            $(this).html(GenerateString(3));
+        }
+    });
+
     function getNumber() {
-
-        // generates the number and remaps from 0 => 1 to -1 => 1
-        let n = (Math.random() * 2) - 1;
-        // multiplies value to get -25=>25
-        n *= 25;
-        // removes decimal points to get integer values
-        n.toFixed(0);
-
-        // checks for 0 value
-        if (Math.abs(n) < 1){
-            // if 0 then redo the calculation
+        //gen value between 0,1 | map to -1,1 | multiply by 25 and round to nearest int
+        let n = Math.round(((Math.random() * 2) - 1) * 25);
+        while (Math.abs(n) < 1) {
             n = getNumber();
         }
-            return n;
+        return Math.round(n);
     }
-    let a = $("td");
-    $.each(a, function () {
-        $(this).html(getNumber().toFixed(0) + " " + getNumber().toFixed(0));
-    })
+    /**
+     * @return {string}
+     */
+    function GenerateSymbol(symbolA, symbolB){
+        let r = Math.round(Math.random());
+        if (r === 1){
+            return symbolA;
+        }
+        return symbolB;
+    }
+    /**
+     * @return {string}
+     */
+    function GenerateString(number) {
+        let equation = "";
+        switch (number) {
 
+            case 1:
+                equation = getNumber() + "x" + GenerateSymbol(" + ",  " - ") + getNumber() + " = " + getNumber() + "x";
+                break;
+            case 2:
+                equation = getNumber() + "(" +getNumber()+ "x" + GenerateSymbol(" + ", " - ") + getNumber() + ")" + GenerateSymbol(" + ", " - ") + getNumber() + " = " + getNumber() + "x";
+                break;
+            case 3:
+                equation = getNumber() + "(" + getNumber() + "x" + GenerateSymbol(" + ", " - ") + getNumber() + ")" + getNumber() + " = " + getNumber() + "(" + getNumber() + "x" + GenerateSymbol(" + ", " - ") + getNumber() + ")";
+                break;
+            default:
+                break;
+        }
+
+        return equation;
+    }
 
     // EQUATION TYPES
     // Ax + Bx = c
     // Ax + B = Cx
 
     //A(Dx + E) + B = Cx
-    //A(Dx + E) + B = Cx
+    //A(D + Ex) + B = Cx
 
+    //A(D + Ex) + B = C(Fx + G)
 
 }
