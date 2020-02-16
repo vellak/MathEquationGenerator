6222b216ece1b98b45f870204730c53a03e65d64
Keith <keithrvell@gmail.com>
2020-02-16 13:28:14 +0100

    reworked RNG values to support negative values between -25 and 25 while still negating '0' value

--- js/mathGenerator.js
+++ js/mathGenerator.js
@@ -2,17 +2,35 @@
 function GenerateNumbers() {
     console.log("Hello");
 
-    //returns a number between 1 and 25
+    //returns a number between -25 and 25, but not 0
     function getNumber() {
-        return (Math.random() * 24) + 1;
+
+        // generates the number and remaps from 0 => 1 to -1 => 1
+        let n = (Math.random() * 2) - 1;
+        // multiplies value to get -25=>25
+        n *= 25;
+        // removes decimal points to get integer values
+        n.toFixed(0);
+
+        // checks for 0 value
+        if (Math.abs(n) < 1){
+            // if 0 then redo the calculation
+            n = getNumber();
+        }
+            return n;
     }
-
-
-
     let a = $("td");
     $.each(a, function () {
-        $(this).html(getNumber().toFixed(0));
+        $(this).html(getNumber().toFixed(0) + " " + getNumber().toFixed(0));
     })
+
+
+    // EQUATION TYPES
+    // Ax + Bx = c
+    // Ax + B = Cx
+
+    //A(Dx + E) + B = Cx
+    //A(Dx + E) + B = Cx
 
 
 }
